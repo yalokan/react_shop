50ce756e1a7bc94967c512e316ba3ba0442c841d
maxstekolshikov <mstekolstsikov@evolutiongaming.com>
2020-01-16 18:29:28 +0200

    add functionality for increase / decrease / remove item from shopping cart

---
+++ build/precache-manifest.84aa134155eef8a5ff54d5e182470aa8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8053607bf9d45ec922e12511f4bda0d2",
+    "url": "/react_shop/index.html"
+  },
+  {
+    "revision": "c6b7154be9a646749fb4",
+    "url": "/react_shop/static/css/main.8be7fe13.chunk.css"
+  },
+  {
+    "revision": "947629eb36f26699b474",
+    "url": "/react_shop/static/js/2.65fa461e.chunk.js"
+  },
+  {
+    "revision": "c6b7154be9a646749fb4",
+    "url": "/react_shop/static/js/main.cfe4effb.chunk.js"
+  },
+  {
+    "revision": "3f81317078ffe239958d",
+    "url": "/react_shop/static/js/runtime-main.90f952df.js"
+  }
+]);
